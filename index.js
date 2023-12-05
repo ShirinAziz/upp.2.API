8d59dbda576b7556e22a7b6608f274bd9ae225c2
ShirinAziz <Shirin.aziz96@outlook.com>
2023-12-05 14:58:55 +0100

    adding functions

--- index.js
+++ index.js
@@ -12,3 +12,40 @@ con = mysql.createConnection({
   password: "",
   database: "dokumentation",
 });
+const COLUMNS = ["id", "username", "password", "name", "email"];
+
+app.get("/users", function (req, res) {
+  let sql = "SELECT * FROM users";
+  let condition = createCondition(req.query);
+  //console.log(sql + condition);
+  con.query(sql + condition, function (err, result, fields) {
+    res.send(result);
+  });
+});
+
+let createCondition = function (query) {
+  //console.log(query);
+  let output = " WHERE ";
+  for (let key in query) {
+    if (COLUMNS.includes(key)) {
+      output += `${key}="${query[key]}" OR `;
+    }
+  }
+  if (output.length == 7) {
+    return "";
+  } else {
+    return output.substring(0, output.length - 4);
+  }
+};
+
+app.get("/users/:id", function (req, res) {
+  let sql = "SELECT * FROM users WHERE id=" + req.params.id;
+  // console.log(sql);
+  con.query(sql, function (err, result, fields) {
+    if (result.length > 0) {
+      res.send(result);
+    } else {
+      res.sendStatus(404);
+    }
+  });
+});
